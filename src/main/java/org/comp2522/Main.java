23dfde4d9305f5c273d7652d8e63c48cc880c888
Eric Dong <xdong17@my.bcit.ca>
2022-09-06 13:22:04 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,13 @@
 package org.comp2522;
 
+/**
+ * A simple class that prints out a greeting message.
+ *
+ * @author Eric Dong
+ * @version 1.0
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Yankees!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, Yankees!");
+  }
 }
